3a358c69cb8daddde9532d86c4e7ca322dc34cb9
adityashaw970 <159451431+adityashaw970@users.noreply.github.com>
2024-12-12 20:52:06 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,30 +11,11 @@ import Project from './Components/Project.jsx';
 import FullProject from './Components/FullProject.jsx';
 import Loader from './Components/Loader.jsx';
 
-// Correct import
-import Lenis from "@studio-freight/lenis";
 
 
 function App() {
 
-  useEffect(()=>{
-    // Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-});
 
-// Listen for the scroll event and log the event data
-lenis.on('scroll', (e) => {
-  console.log(e);
-});
-// Use requestAnimationFrame to continuously update the scroll
-function raf(time) {
-  lenis.raf(time);
-  requestAnimationFrame(raf);
-}
-
-requestAnimationFrame(raf);
-  })
 
   return (
     <>
